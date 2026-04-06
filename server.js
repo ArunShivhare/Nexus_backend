2ab646b34effc6d89c47373b95d14c8060cdbb04
Arun Shivhare <arunshivhare925@gmail.com>
2026-04-06 19:27:02 +0530

    final chat fix

--- server.js
+++ server.js
@@ -46,34 +46,28 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log("⚡ User connected:", socket.id);
 
-  // Join task room
   socket.on("joinTask", (taskId) => {
     socket.join(taskId);
     console.log(`📌 Joined room: ${taskId}`);
   });
 
-  // Send message
-  socket.on("sendMessage", ({ taskId, message }) => {
-    io.to(taskId).emit("receiveMessage", {
-      ...message,
-      taskId, // 🔥 include this
-    });
+  socket.on("sendMessage", async ({ taskId, message }) => {
+    try {
+      const newMsg = await Message.create({
+        taskId,
+        sender: message.sender,
+        text: message.text,
+        time: message.time,
+      });
+
+      io.to(taskId).emit("receiveMessage", newMsg);
+    } catch (err) {
+      console.error("Error saving message:", err);
+    }
   });
 
   socket.on("disconnect", () => {
     console.log("❌ User disconnected:", socket.id);
-  });
-
-  socket.on("sendMessage", async ({ taskId, message }) => {
-    // save in DB
-    const newMsg = await Message.create({
-      taskId,
-      sender: message.sender,
-      text: message.text,
-      time: message.time,
-    });
-
-    io.to(taskId).emit("receiveMessage", newMsg);
   });
 });
 
